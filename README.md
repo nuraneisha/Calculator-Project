9642a6eb312fee906b73cb655a939e393ebc6718
nuraneisha <git@stackblitz.com>
2025-05-22 13:54:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator-Project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nuraneisha/Calculator-Project)
---
+++ script.js
@@ -0,0 +1,27 @@
+function add() {
+  const x = parseInt(prompt('Please enter first integer'));
+  const y = parseInt(prompt('please enter sec number'));
+  const sum = x + y;
+  alert(`The value are : ${sum}`);
+}
+
+function subtract() {
+  const x = parseInt(prompt('Please enter first integer'));
+  const y = parseInt(prompt('please enter sec number'));
+  const sum = x - y;
+  alert(`The value are : ${sum}`);
+}
+
+function divide() {
+  const x = parseInt(prompt('Please enter first integer'));
+  const y = parseInt(prompt('please enter sec number'));
+  const sum = x / y;
+  alert(`The value are : ${sum}`);
+}
+
+function multiply() {
+  const x = parseInt(prompt('Please enter first integer'));
+  const y = parseInt(prompt('please enter sec number'));
+  const sum = x * y;
+  alert(`The value are : ${sum}`);
+}
